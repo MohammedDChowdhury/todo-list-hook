22b202bee6fe9ab1df90f0868d62ff4d1a4673fe
Mohammed <d2018c@gmail.com>
2021-12-27 15:37:11 +0000

    todo list with hook usestate, managed to output tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import Form from "./Form";
+import React, { useState } from "react";
+export default () => {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        onSubmit={(text) => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text }) => (
+          <div key={text}>{text}</div>
+        ))}
+      </div>
     </div>
   );
-}
-
-export default App;
+};
